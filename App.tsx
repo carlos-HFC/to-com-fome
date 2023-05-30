a32ed71db1c70209150c4f7e35afe96ca429bdba
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-05-30 18:21:42 -0300

    adicionar fonte

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
+import { Montserrat_700Bold, useFonts } from "@expo-google-fonts/montserrat";
 import { StatusBar } from 'react-native';
 
 import { Routes } from "./src/routes";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Montserrat: Montserrat_700Bold,
+  });
+
+  if (!loaded) return null;
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,85 @@
-export const USER = "@to-com-fome:user"
+export const USER = "@to-com-fome:user";
+
 export const COLORS = {
-  green: '#32B768',
   white: '#FFFFFF',
+  greenLight: '#32B768',
+  green: "#279B56",
+  greenDark: "#0f4400",
+  red: "#FF0000",
+  dark: "#000000"
 };
-export const POINTS = [
-  { latitude: -23.5657344, longitude: -46.6538182, title: "Av. Paulista" },
-  { latitude: -23.8528971, longitude: -46.9182845, title: "Cidade do Fernando" },
-  { latitude: -23.5586759, longitude: -46.4780569, title: "Cesta da Economia" },
+
+export const POINTS: Point[] = [
+  {
+    title: "Cesta da Economia",
+    email: "cestadaeconomia@email.com",
+    phone: "11964096868",
+    cep: "08280-190",
+    address: "Av. Maria Luiza Americano, 2741",
+    district: "Cidade Líder",
+    city: "São Paulo",
+    uf: "SP",
+    latitude: -23.5586759,
+    longitude: -46.4780569
+  },
+  {
+    title: "Caio",
+    email: "caio@email.com",
+    phone: "11959836637",
+    cep: "08295-005",
+    address: "Av. Miguel Ignácio Curi, 111",
+    district: "Artur Alvim",
+    city: "São Paulo",
+    uf: "SP",
+    latitude: -23.5457527,
+    longitude: -46.4741651
+  },
+  {
+    title: "Gustavo",
+    email: "gustavo@email.com",
+    phone: "11942659313",
+    cep: "09390-040",
+    address: "Av. Gov. Mario Covas Júnior, 01",
+    district: "Centro",
+    city: "Mauá",
+    uf: "SP",
+    latitude: -23.6643151,
+    longitude: -46.4632126
+  },
+  {
+    title: "Eder",
+    email: "eder@email.com",
+    phone: "11957850623",
+    cep: "04263-000",
+    address: "Parque da Independência",
+    district: "Ipiranga",
+    city: "São Paulo",
+    uf: "SP",
+    latitude: -23.5870398,
+    longitude: -46.6131051
+  },
+  {
+    title: "Fernando",
+    email: "fernando@email.com",
+    phone: "11995480607",
+    cep: "09600-000",
+    address: "R. Serafim Luís Martins, 13",
+    district: "Vila Louro",
+    city: "Embu-Guaçu",
+    uf: "SP",
+    latitude: -23.8308174,
+    longitude: -46.8160316
+  },
 ];
+
+export const SHADOW = {
+  shadowOffset: { width: 0, height: 4 },
+  shadowColor: COLORS.dark,
+  shadowOpacity: 0.25,
+  shadowRadius: 8,
+  elevation: 8
+};
+
+export const FONTS = {
+  Montserrat: "Montserrat"
+}
